19f97f987f1f02bfd61664bee115769a345878d3
SammriddhGupta <sammriddhgupta@gmail.com>
2023-02-03 00:36:55 +0800

    add start end timer

--- app.py
+++ app.py
@@ -13,3 +13,25 @@ st.write("""
 
 button_clicked = st.button("Start")
 
+t1 = 1500
+t2 = 300
+
+if button_clicked:
+    with st.empty():
+        while t1:
+            mins, secs = divmod(t1,60)
+            timer = '{:02d}:{:02d}'.format(mins,secs)
+            st.header(f"⏳{timer}")
+            time.sleep(1)
+            t1 -= 1
+            st.success("🥳 25 minutes are up! Now take a well deserved break :) ")
+            
+    with st.empty():
+        while t2:
+            mins2, secs2 = divmod(t2,60)
+            timer2 = '{:02d}:{:02d}'.format(mins2,secs2)
+            st.header(f"😇{timer2}")
+            time.sleep(1)
+            t2 -= 1
+            st.error("⏲ Chop Chop back to work time, 5 minutes are up ")
+                        
